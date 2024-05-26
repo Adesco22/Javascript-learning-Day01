06cf4acc48a17a2aab53fc2c3f44f3ac171ff119
Adeyefa Emmanuel <adescoemmanuel@gmail.com>
2024-05-26 01:55:30 +0100

    files are updated, Javascript Learning for Day 01

--- Day_01_exercise.js
+++ Day_01_exercise.js
@@ -1,31 +1,56 @@
 // 01. comments can make code readable
 
-//02. Welcome to 30DaysOfJavaScript
+// 02. Welcome to 30DaysOfJavaScript
 
 /* 
-03. comments can make code readable, easy to reuse and informative
+03. A multiline comment can make code readable, easy to reuse and informative
  */
 
+
 // 04. declaring variables and assigning string, boolean, undefined and null data types
-let age = 12; // integer
+let evenNumber = 12; // integer
 
 let name = "Mary"; // string
 
 let GoingToSchool = true; // boolean
 
 let zero = undefined; // undefined
+
 let infinity = null;  // null
 
-//05. Creating type of operators
-console.log(typeof age)
+//05. Using typeof operator to check different data types
+console.log(typeof evenNumber)
 console.log(typeof GoingToSchool)
 console.log(typeof name)
 console.log(typeof zero)
 console.log(typeof infinity)
 
-//06. declaring variables in multiple lines
+// 06. Declaring of variables without assigning values
+var fruits;
+var Slogan;
+var cities;
+var color;
+
+// 07. Declaring variables with assigned values
+let city = "Lagos";
+let date = "26th of 2024";
+let time = `1:37 am`;
+let randomNumber = 12;
+
+//08. declaring variables in multiple lines
 
 let firstName = "Emmanuel";
 let lastName = "Adeyefa";
+let maritalStatus = "Single";
+let country = "Nigeria";
+let age = 26;
 
+//09. Declaring variables im single line
+const fn = "Emmanuel", sn = "Adeyefa", marital_status = "Single", my_country = "Nigeria", my_age = 26;
+console.log(my_age)
 
+//10. initalization of my age and your age
+let MyAge = 25;
+let yourAge = 19;
+console.log(MyAge)
+console.log(yourAge)
