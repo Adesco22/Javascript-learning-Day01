c5ddb89c5d9c5e56bacba62796b452bc2a76dc15
Adeyefa Emmanuel <adescoemmanuel@gmail.com>
2024-05-26 01:16:53 +0100

    first commit

---
+++ Day_01_practice_file/Day_01_exercise.js
@@ -0,0 +1,34 @@
+// 01. comments can make code readable
+
+//02. Welcome to 30DaysOfJavaScript
+
+/* 
+03. comments can make code readable, easy to reuse and informative
+ */
+
+// 04. declaring variables and assigning string, boolean, undefined and null data types
+let age = 12; //integer
+
+let name = "Mary"; //string
+
+let GoingToSchool = true; // bolean
+
+let zero = undefined; // udefined
+let infinity = null;  // null
+
+//05. Creating type of operators
+
+console.log(typeof age)
+console.log(typeof GoingToSchool)
+console.log(typeof name)
+console.log(typeof zero)
+console.log(typeof infinity)
+
+//dec
+
+// declaring variables in multiple lines
+
+let firstName = "Emmanuel";
+let lastName = "Adeyefa";
+
+
